79a68476776645a1ea70700502a5aed837b7f42a
Adam <rr-adam@users.noreply.github.com>
2023-09-01 15:33:40 +0200

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,17 @@
+<script>
+  import { base } from '$app/paths';
+
+  let count = 0;
+  
+  function handleClick() {
+  count += 1;
+  }
+</script>
+
+<button on:click={handleClick}>
+	clicks: {count}
+</button>
+
+<p>
+  <a href="{base}/subpage">Go to subpage</a>
+</p>
---
+++ src/routes/subpage/+page.svelte
@@ -0,0 +1,7 @@
+<script>
+  import { base } from '$app/paths';
+</script>
+
+This is a subpage!
+
+<a href="{base}/">Go back to homepage</a>
