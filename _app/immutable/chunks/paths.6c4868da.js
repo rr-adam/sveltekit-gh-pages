89884a94c328c152e0a81509db652175a9bf8cce
rr-adam <87621210+rr-adam@users.noreply.github.com>
2023-09-01 13:34:33 +0000

    Deploying to gh-pages from @ rr-adam/sveltekit-gh-pages@79a68476776645a1ea70700502a5aed837b7f42a 🚀

---
+++ _app/immutable/chunks/paths.6c4868da.js
@@ -0,0 +1,1 @@
+var s;const t=((s=globalThis.__sveltekit_18k2k1e)==null?void 0:s.base)??"/sveltekit-gh-pages";var e;const a=((e=globalThis.__sveltekit_18k2k1e)==null?void 0:e.assets)??t;export{a,t as b};
---
+++ _app/immutable/nodes/3.7e00fe4c.js
@@ -0,0 +1,5 @@
+import{s as m,n as s}from"../chunks/scheduler.e108d1fd.js";import{S as h,i as p,m as c,g as f,n as u,h as x,y as _,k as b,a as i,f as r}from"../chunks/index.7e6319f2.js";import{b as g}from"../chunks/paths.6c4868da.js";function d(l){let a,t,n="Go back to homepage";return{c(){a=c(`This is a subpage!
+
+`),t=f("a"),t.textContent=n,this.h()},l(e){a=u(e,`This is a subpage!
+
+`),t=x(e,"A",{href:!0,"data-svelte-h":!0}),_(t)!=="svelte-1lrbpw2"&&(t.textContent=n),this.h()},h(){b(t,"href",g+"/")},m(e,o){i(e,a,o),i(e,t,o)},p:s,i:s,o:s,d(e){e&&(r(a),r(t))}}}class y extends h{constructor(a){super(),p(this,a,null,d,m,{})}}export{y as component};
